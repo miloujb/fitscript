7b55d0ae6157503cb4b8bce05f17563aa8dc21fb
miloujb <emilyjbailey@hotmail.co.uk>
2023-06-06 00:27:21 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import {
+    useEffect,
+    useState
+} from 'react'
 import Navbar from './scenes/Navbar'
 import {
     SelectedPage
@@ -8,10 +11,25 @@ function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(
         SelectedPage.Home
     );
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage(SelectedPage.Home);
+            } else {
+                if (window.scrollY !== 0) {
+                    setIsTopOfPage(false);
+                }
+                window.addEventListener("scroll", handleScroll)
+                return () => window.removeEventListener("scroll", handleScroll)
+                    , []}
+        }
+    })
     return (
         <div className="App bg-gray-20">
-            <Navbar selectedPage={selectedPage} setSelectedPage={selectedPage}/>
+            <Navbar selectedPage={selectedPage} setSelectedPage={selectedPage} isTopOfPage={isTopOfPage}/>
         </div>
   )
 }
--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -1,28 +1,45 @@
-import {useState} from "react";
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
-import Logo from "../../assets/fitscript-images/Logo.png";
+import {
+    useEffect,
+    useState
+} from "react";
+import {
+    Bars3Icon,
+    XMarkIcon
+} from "@heroicons/react/24/solid"
+import Logo
+    from "../../assets/fitscript-images/Logo.png";
 import Link
     from "./Link";
 import {
     SelectedPage
 } from "../../shared/types";
+import useMediaQuery
+    from "../../hooks/useMediaQuery";
+import ActionButton
+    from "../../shared/ActionButton";
 
 type Props = {
     page: string
     selectedPage: SelectedPage,
     setSelectedPage: (value: SelectedPage) => void
+    isTopOfPage: boolean
 }
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({selectedPage, setSelectedPage, isTopOfPage}: Props) => {
     const flexBetween = "flex items-center justify-between"
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+
     return (
         <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
                 <div className={`${flexBetween} mx-auto w-5/6`}>
                     <div className={`${flexBetween} w-full gap-16`}>
                         <img alt="logo" src={Logo} />
                     </div>
-                    <div className={`${flexBetween} w-full`}>
+                    {isAboveMediumScreens ? (
+                        <div className={`${flexBetween} w-full`}>
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link page="Home" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
                             <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
@@ -31,11 +48,32 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                         </div>
                         <div className={`${flexBetween} gap-8`}>
                             <p>Sign In</p>
-                            <button>Become A Member</button>
+                            <ActionButton setSelectedPage={setSelectedPage}>Become A Member</ActionButton>
                         </div>
                     </div>
+                    ) : (
+                        <button className="rounded-full bg-secondary-500 p-2"
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <Bars3Icon className="h-6 w-6 text-white"/>
+                        </button>
+                    )}
                 </div>
             </div>
+            {!isAboveMediumScreens && isMenuToggled && (
+                <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                    <div className="flex justify-end p-12">
+                        <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
+                        <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
+                        <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
+                        <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={selectedPage}/>
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
